117225121894cf55d57f6c03fb4ea840183ed4b8
kennedybryant <kennedy_marisa96@yahoo.com>
2021-11-03 16:28:19 -0400

    checkById Middleware

--- api/server.js
+++ api/server.js
@@ -1,10 +1,20 @@
-const express = require('express');
+const express = require('express')
 
-const SchemeRouter = require('./schemes/scheme-router.js');
+const SchemeRouter = require('./schemes/scheme-router.js')
 
-const server = express();
+const server = express()
 
-server.use(express.json());
-server.use('/api/schemes', SchemeRouter);
+server.use(express.json())
+server.use('/api/schemes', SchemeRouter)
 
-module.exports = server;
+server.use('*', (req, res, next) => {
+    next({ status: 404, message: 'not found' })
+})
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
+module.exports = server
